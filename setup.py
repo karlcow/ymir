d8fb8345fa4c4146f7db2f991a191cf4200d3d59
karl <karl+github@la-grange.net>
2012-06-08 23:23:41 -0400

    Added metadata to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='ymir',
+    name='Ymir',
+    author='Karl Dubost',
+    author_email='karl+ymir@la-grange.net',
     version='0.1dev',
     packages=['ymir', ],
-    license='MIT',
+    license='LICENSE.txt',
+    url='http://pypi.python.org/pypi/Ymir/',
+    description='script to manage the La Grange blog http://www.la-grange.net/',
     long_description=open('README.txt').read(),
 )
