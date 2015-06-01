f697808a3418c8701e1e4f47c220bc918cf55acc
Karl Dubost <karl+github@la-grange.net>
2015-06-01 17:50:45 +0900

    Let s put the pipes here

---
+++ ymir/utils/plumbing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+'''
+plumbing.py
+
+Created by Karl Dubost on 2015-06-01
+Copyright (c) 2015 Grange. All rights reserved.
+see LICENSE.TXT
+'''
+
+
+def new_post():
+    '''Create a template for a new blog post.'''
+    pass
