3911c56e7025e6c8c87715227f43d8d7737d19ac
Karl Dubost <karl+github@la-grange.net>
2015-06-01 18:56:17 +0900

    starting to move some features. now date

--- ymir/utils/helper.py
+++ ymir/utils/helper.py
@@ -8,10 +8,12 @@ Copyright (c) 2014 Grange. All rights reserved.
 see LICENSE.TXT
 '''
 
+import locale
 import os
+import re
 import urllib2
 import urlparse
-import re
+import sys
 
 ROOT = '/Users/karl/Sites/la-grange.net/'
 WEB = 'http://lagrange.test.site/'
@@ -77,8 +79,38 @@ def on_this_day(uri_list, month='01', day='01'):
             title = extract_title(text)
             onthisday_list[urlparse.urljoin(GRANGE, path)] = title
     return onthisday_list
-onthisday_list = on_this_day(BLOGPOST_LIST_URI, '02', '02')
-print onthisday_list
-for uri in onthisday_list.iterkeys():
-    print '''* {0}
-  {1}'''.format(onthisday_list[uri], uri)
+# onthisday_list = on_this_day(BLOGPOST_LIST_URI, '02', '02')
+# print onthisday_list
+# for uri in onthisday_list.iterkeys():
+#     print '''* {0}
+#   {1}'''.format(onthisday_list[uri], uri)
+
+
+def nowdate(date_time, format=""):
+    '''Compute date in different date string formats.'''
+    # date in French please
+    my_locale = "fr_FR"
+    locale.setlocale(locale.LC_ALL, my_locale)
+    if format == "rfc3339":
+        return date_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    elif format == "iso":
+        return date_time.strftime("%Y-%m-%d")
+    elif format == "path":
+        return date_time.strftime("%Y/%m/%d")
+    elif format == "humain":
+        # remove the leading 0 of the date
+        dategeek = date_time.strftime("%d %B %Y")
+        if dategeek.startswith('0'):
+            dategeek = dategeek.lstrip('0')
+        return dategeek
+    elif format == "humainlong":
+        # Remove the leading 0
+        # And add the day of the week
+        # "Vendredi "+ "3 février 2012"
+        dategeek = date_time.strftime("%d %B %Y")
+        if dategeek.startswith('0'):
+            dategeek = dategeek.lstrip('0')
+        return date_time.strftime("%A ") + dategeek
+    else:
+        print("wrong format")
+        sys.exit(1)
