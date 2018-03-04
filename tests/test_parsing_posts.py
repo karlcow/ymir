07f16dfddbdb42fb3471fb43d8bc1558b93ac0cd
Karl Dubost <karl+github@la-grange.net>
2018-03-04 08:03:36 +0100

    Adds tests for content extraction

--- tests/test_parsing_posts.py
+++ tests/test_parsing_posts.py
@@ -6,8 +6,11 @@ Parsing Blog Posts Unit Tests.
 see LICENSE.TXT
 """
 
+from io import BytesIO
 import os
 import unittest
+
+from lxml import etree
 
 from ymir.utils import parsing
 
@@ -21,6 +24,12 @@ class TestYmirParsing(unittest.TestCase):
         """Read the fixture for tests."""
         fixture_path = os.path.abspath(os.path.join(FIXTURE_DIR, fixture_file))
         return parsing.parse_html_post(fixture_path)
+
+    def make_xml(self, text):
+        """Convert a string as an etree Element."""
+        parser = etree.XMLParser(remove_blank_text=True)
+        xml_fragment = etree.parse(BytesIO(text), parser)
+        return xml_fragment.getroot()
 
     def setUp(self):
         """Set up the tests."""
@@ -54,3 +63,27 @@ class TestYmirParsing(unittest.TestCase):
         self.assertEqual(
             ctx.exception.message,
             "date type must be one of ['modified', 'created']")
+
+    def test_get_content(self):
+        """Test the extraction of the content."""
+        # normal case
+        expected = '<html:article xmlns:html="http://www.w3.org/1999/xhtml"><html:p>This is content: &#28961;</html:p></html:article>'  # noqa
+        doc = self.read_fixture('content-simple.html')
+        content = parsing.get_content(doc)
+        self.assertEqual(
+            etree.tostring(self.make_xml(expected), with_tail=False),
+            etree.tostring(content).replace('\n', ''))
+        # no header in the content
+        expected = '<html:article xmlns:html="http://www.w3.org/1999/xhtml"><html:p>content without header</html:p></html:article>'  # noqa
+        doc = self.read_fixture('content-no-header.html')
+        content = parsing.get_content(doc)
+        self.assertEqual(
+            etree.tostring(self.make_xml(expected), with_tail=False),
+            etree.tostring(content).replace('\n', ''))
+        # there is no article
+        doc = self.read_fixture('content-none.html')
+        with self.assertRaises(IndexError) as ctx:
+            parsing.get_content(doc)
+        self.assertEqual(
+            ctx.exception.message,
+            "Ooops. No article.")
